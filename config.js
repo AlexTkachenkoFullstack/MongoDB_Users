8b8ad77336bdd7073edceba6ff9f54c2b65ef165
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-08-06 10:53:55 +0100

    move DB_HOST to process.env

---
+++ config.js
@@ -0,0 +1,7 @@
+
+// добавляем пароль и перед ? добавляем название базы данных, к которой хотим подключиться
+const DB_HOST='mongodb+srv://Alex:kKHXDDRWBhcU62PJ@cluster0.u3myxu4.mongodb.net/users_reader?retryWrites=true&w=majority'
+
+module.exports={
+    DB_HOST
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Alex
 // kKHXDDRWBhcU62PJ
-
+const {DB_HOST}=process.env
 const app = require('./app')
 // импортируем библиотеку для работы с Mongodb
 const mongoose = require('mongoose');
 // добавляем пароль и перед ? добавляем название базы данных, к которой хотим подключиться
-const DB_HOST='mongodb+srv://Alex:kKHXDDRWBhcU62PJ@cluster0.u3myxu4.mongodb.net/users_reader?retryWrites=true&w=majority'
+// const DB_HOST='mongodb+srv://Alex:kKHXDDRWBhcU62PJ@cluster0.u3myxu4.mongodb.net/users_reader?retryWrites=true&w=majority'
 // запускаем метод библиотеки
 mongoose.connect(DB_HOST)
 .then(()=>{
